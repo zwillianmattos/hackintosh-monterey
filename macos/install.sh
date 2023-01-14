d85a302326f16b1231904f03671a700904ee5aeb
Willian de Mattos Silva <willian_matt@hotmail.com>
2023-01-13 21:33:04 -0300

    fix: ajust files folder

--- macos/install.sh
+++ macos/install.sh
@@ -2,4 +2,4 @@ cd com.apple.recovery.boot
 
 echo "# Monterey (12)"
 
-python3 macrecovery.py -b Mac-FFE5EF870D7BA81A -m 00000000000000000 download
+python3 ./macrecovery/macrecovery.py -b Mac-FFE5EF870D7BA81A -m 00000000000000000 download
